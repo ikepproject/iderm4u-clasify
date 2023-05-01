3b1eb1d0ae84817dd013d13e5fae69cc9089ecbb
ikepproject <iketuteddy.project@gmail.com>
2023-05-01 17:32:09 +0700

    try to fix cors 1

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-const express = require('express');
-const tf = require('@tensorflow/tfjs-node');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const tf = require("@tensorflow/tfjs-node");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
-
-const MODEL_PATH = 'model/model.json';
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
+app.use(cors());
+const MODEL_PATH = "model/model.json";
 let model;
 
 async function loadModel() {
-  model = await tf.loadLayersModel('file://' + MODEL_PATH);
-  console.log('Model loaded successfully');
+  model = await tf.loadLayersModel("file://" + MODEL_PATH);
+  console.log("Model loaded successfully");
 }
 
 loadModel();
 
-app.use('/images', express.static(__dirname + '/public/images'));
+app.use("/images", express.static(__dirname + "/public/images"));
 
-app.post('/classify', async (req, res) => {
+app.post("/classify", async (req, res) => {
   try {
-    const response = await axios.get(req.body.imageUrl, { responseType: 'arraybuffer' });
-	const imageBuffer = Buffer.from(response.data, "binary");
+    const response = await axios.get(req.body.imageUrl, {
+      responseType: "arraybuffer",
+    });
+    const imageBuffer = Buffer.from(response.data, "binary");
     const tfimage = tf.node.decodeImage(imageBuffer);
     const tfresized = tf.image.resizeBilinear(tfimage, [224, 224]);
     const tfexpanded = tfresized.expandDims(0);
@@ -44,7 +46,7 @@ app.post('/classify', async (req, res) => {
       .slice(0, 3)
       .map(({ score, index }) => ({
         class: getClass(index),
-        score: Math.round(score * 100)
+        score: Math.round(score * 100),
       }));
 
     res.send(top3);
@@ -55,11 +57,11 @@ app.post('/classify', async (req, res) => {
 });
 
 function getClass(index) {
-  const classes = require('./model/skin_classes.json');
+  const classes = require("./model/skin_classes.json");
   return classes[index];
 }
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 const PORT = process.env.PORT || 3000;
 
