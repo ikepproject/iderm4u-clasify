6a053932291f7b92d2a29bf518f6cc01e65abb6f
ikepproject <iketuteddy.project@gmail.com>
2023-05-01 17:10:48 +0700

    update1 server.js enable all origin

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 app.use(express.json());
 
