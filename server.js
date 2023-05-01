17eaa9a51f136fab633b6fd4e91aa578112273e8
ikepproject <iketuteddy.project@gmail.com>
2023-05-01 17:27:56 +0700

    fix cors error again

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const cors = require('cors');
 const axios = require("axios");
 
 const app = express();
-
-//app.use(cors());
+app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
-
-app.use(express.json());
 
 const MODEL_PATH = 'model/model.json';
 let model;
