0e4d843634fe9ce3e19520353db973f96316f944
ikepproject <iketuteddy.project@gmail.com>
2023-05-01 17:05:08 +0700

    add cors to server.js, enable all origin

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
